cd8934d2728ed54b234d9e2d04f9adde672bf2a7
Antoine Thomas <antoinethomas.devweb@gmail.com>
2022-08-14 20:01:39 +0200

    adedd php

--- contact.php
+++ contact.php
@@ -7,11 +7,13 @@ if (isset($_POST['message'])) {
 
         $message = '<h1>Message envoyé depuis la page Contact de meuzeretlaurence.com</h1>
                     <p><b>Email : </b>' . $_POST['email'] . '<br>
+                    <p><b>Prénom : </b>' . $_POST['firstname'] . '<br>
+                    <p><b>Nom : </b>' . $_POST['lastname'] . '<br>
                     <p><b>Numéro : </b>' . $_POST['phone'] . '<br>
                     <b>Message : </b>' . htmlspecialchars($_POST['message']) . '</p>';
 
         $retour = mail('antoinethomas.devweb@gmail.com', 'Envoi depuis page Contact', $message, $entete);
         if($retour)
-            echo "<p>Votre message a bien été envoyé.</p>";
+            echo "<h1 style='margin-top: 100px auto; padding: 30px'>J'ai bien reçu votre demande. </br> Je vous recontacte au plus vite, <br/> </br> Laurence Meuseret</br></br></br> <a href='http://meuzeretlaurence.com'>Revenir à la page d'acceuil</a></h1> ";
     }
  
